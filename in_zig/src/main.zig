374d2921320499ee5548fd563c61ab4b4236cea2
Sezoka <vlad.novport@gmail.com>
2023-06-09 21:20:54 +0300

    3 zigs more

--- in_zig/src/main.zig
+++ in_zig/src/main.zig
@@ -70,8 +70,8 @@ fn slice_stdin_by_words() void {
     }
 }
 
-// 1.10
-fn stdin_words_length_vertical_whistogram() void {
+// 1.13
+fn stdin_words_length_vertical_histogram() void {
     const max_len = 256;
 
     var word_lengths_cnt = [_]u16{0} ** max_len;
@@ -138,10 +138,94 @@ fn stdin_words_length_vertical_whistogram() void {
     writer.print("# = {d:.3} char/s\n", .{x_step}) catch {};
 }
 
+// 1.14
+fn stdin_chars_histogram() void {
+    var chars = [_]u32{0} ** 256;
+
+    while (reader.readByte() catch null) |byte| {
+        if (!is_whitespace(byte))
+            chars[byte] += 1;
+    }
+
+    var max_len: u32 = 0;
+    for (chars) |cnt| {
+        if (max_len < cnt) {
+            max_len = cnt;
+        }
+    }
+
+    const max_width = 80.0;
+    const step = @intToFloat(f32, max_len) / max_width;
+
+    for (chars, 0..) |cnt, char| {
+        if (cnt == 0 or char < '!') continue;
+        const width = @intToFloat(f32, cnt);
+        var x: f32 = 0.0;
+        var i: u16 = 0;
+        writer.print("{d: >4} |", .{cnt}) catch {};
+        while (x <= width) : (x += step) {
+            writer.writeByte(@intCast(u8, char)) catch {};
+            i += 1;
+        }
+        while (i <= max_width) : (i += 1) {
+            writer.writeByte(' ') catch {};
+        }
+        writer.writeAll("|\n") catch {};
+    }
+}
+
+// 1.17
+fn print_lines_longer_than_80() void {
+    var buff = [_]u8{0} ** 512;
+    while (reader.readUntilDelimiter(&buff, '\n') catch null) |line| {
+        if (80 < line.len) {
+            writer.writeAll(line) catch return;
+            writer.writeByte('\n') catch return;
+        }
+    }
+}
+
+// 1.18
+fn trim_lines_end_of_stdin() void {
+    var buff = [_]u8{0} ** 512;
+    while (reader.readUntilDelimiter(&buff, '\n') catch null) |line| {
+        if (line.len < 1) continue;
+        var line_end = line.len - 1;
+        while (0 < line_end and is_whitespace(line[line_end])) : (line_end -= 1) {}
+        if (!is_whitespace(line[line_end])) {
+            writer.writeAll(line[0 .. line_end + 1]) catch return;
+            writer.writeByte('\n') catch return;
+        }
+    }
+}
+
+// 1.19
+fn reverse_stdin_lines() void {
+    var buff = [_]u8{0} ** 512;
+    while (reader.readUntilDelimiter(&buff, '\n') catch null) |line| {
+        reverse(line);
+        writer.writeAll(line) catch return;
+        writer.writeByte('\n') catch return;
+    }
+}
+
+fn reverse(str: []u8) void {
+    var i: usize = 0;
+    while (i < str.len / 2) : (i += 1) {
+        const j = str.len - i - 1;
+        const temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+}
+
 pub fn main() !void {
     // whitespace_counter();
     // dedup_spaces();
     // escape_stdin();
     // slice_stdin_by_words();
-    stdin_words_length_vertical_whistogram();
+    // stdin_words_length_vertical_histogram();
+    // stdin_chars_histogram();
+    // trim_lines_end_of_stdin();
+    reverse_stdin_lines();
 }
